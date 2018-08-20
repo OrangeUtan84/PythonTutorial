ab2288bd11345d3de9374ef086bc8373bce1fb6d
Martin <foeller.martin@googlemail.com>
2018-08-20 10:09:21 +0200

    added some more example files...

---
+++ list_stackexample.py
@@ -0,0 +1,12 @@
+stack = [3,4,5]
+stack.append(7)
+stack.append(4)
+
+print(stack)
+
+print('Removed from stack:', stack.pop())
+print('Removed from stack:', stack.pop())
+print('Removed from stack:', stack.pop())
+print('Removed from stack:', stack.pop())
+
+print(stack)
